97d875613f16cc0994b1fed4ecc611cb9e909784
François-Xavier Thomas <fx.thomas@gmail.com>
2018-07-16 22:57:13 +0200

    Settings: Do not crash if THEME_CONTACT_EMAIL is not defined

--- irsp/settings/__init__.py
+++ irsp/settings/__init__.py
@@ -1,6 +1,11 @@
+THEME_CONTACT_EMAIL = None
 from .default import *
 try:
     from .local import *
 except ImportError as e:
     pass
-THEME_CONTACT_EMAIL = DEFAULT_FROM_EMAIL
+if not THEME_CONTACT_EMAIL:
+    try:
+        THEME_CONTACT_EMAIL = DEFAULT_FROM_EMAIL
+    except NameError:
+        pass
