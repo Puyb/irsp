e51e0703095458f174512d7cccf02818e914ae25
François-Xavier Thomas <fx.thomas@gmail.com>
2018-07-16 23:11:12 +0200

    Add proper Discourse SSO support

---
+++ discourse_django_sso/README.md
@@ -0,0 +1,6 @@
+Django Discourse SSO package
+============================
+
+From [django-discourse-sso](https://bitbucket.org/olcms/django-discourse-sso)
+0.9.1 (commit b5384aab), with additional patches for handling Django 2.0,
+better unicode support and some bug fixes with newer Discourse versions.
--- irsp/urls.py
+++ irsp/urls.py
@@ -4,50 +4,29 @@ from django.views.generic import TemplateView
 from discourse_django_sso import views as sso_views
 from . import settings
 
-
-class FixedInMemoryNonceService(sso_views.InMemoryNonceService):
-
-    def __init__(self):
-        super().__init__()
-        self.nonce_val = "e15917e0320992596f638ebff7678202"
-
-    def generate_nonce(self) -> str:
-        self.generated_nonces.add(self.nonce_val)
-        return self.nonce_val
-
-    def fixed_nonce_val(self):
-        return self.nonce_val
-
-    def clear_invalid_nonces(self):
-        self.invalid_nonces.clear()
-
-nonce_service = FixedInMemoryNonceService()
+# Nonce generator service, used for authenticating with the Discourse SSO provider
+nonce_service = sso_views.InMemoryNonceService()
 
 urlpatterns = [
     path('member/', include('members.urls')),
     path('admin/', admin.site.urls),
     path(
-        'sso/',
-        sso_views.SSOProviderView.as_view(
-            sso_redirect=settings.DISCOURSE_SSO_REDIRECT,
-            sso_secret=settings.DISCOURSE_SSO_KEY
-        ),
-        name="sso"
-    ),
-    path(
         'accounts/login/',
         sso_views.SSOClientView.as_view(
-            sso_secret=settings.DISCOURSE_SSO_KEY,
-            sso_url=settings.DISCOURSE_SSO_URL,
-            nonce_service=nonce_service),
-        name="client"
+            sso_secret=settings.DISCOURSE_SSO_KEY,             # Discourse SSO key
+            sso_url=settings.DISCOURSE_SSO_URL,                # Discourse SSO URL
+            sso_redirect_url=settings.DISCOURSE_SSO_REDIRECT,  # Local redirect URL
+            nonce_service=nonce_service,
+        ),
+        name="accounts-login"
     ),
     path(
-        'createSession/',
+        'accounts/sso-create-session/',
         sso_views.SSOCreateSessionView.as_view(
             sso_secret=settings.DISCOURSE_SSO_KEY,
-            nonce_service=nonce_service),
-        name="createSession"
+            nonce_service=nonce_service,
+        ),
+        name="accounts-sso-create-session"
     ),
     path("", TemplateView.as_view(template_name="homepage.html"), name="home"),
 ]
